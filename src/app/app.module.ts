ade67d1337bde248a6cbf099eccaf9ab2dfc5112
Jing Chai <jingweichai@gmail.com>
2017-06-11 23:34:34 -0400

    Add better loading of Material Icons and HammerJS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import 'hammerjs';
 
 // Modules
 import { AppRoutingModule } from './app-routing.module';
