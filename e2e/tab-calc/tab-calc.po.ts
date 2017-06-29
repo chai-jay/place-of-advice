190c473ecf736bb47134b73381d2c31bd4fa35b1
Jing Chai <jingweichai@gmail.com>
2017-06-29 19:44:05 -0400

    Add e2e test for Tab Calc page's header and inputs.

--- e2e/tab-calc/tab-calc.po.ts
+++ e2e/tab-calc/tab-calc.po.ts
@@ -5,11 +5,63 @@ export class TabCalcPage {
     return browser.get('/tab-calc');
   }
 
-  getTabCalcElement() {
+  getCalcElement() {
     return element(by.css('pla-tab-calc'));
   }
 
-  getTabCalcMdcardElement() {
+  getCalcMdcardElement() {
     return element(by.css('pla-tab-calc md-card'));
+  }
+
+  getCalcHeaderText() {
+    return element(by.css('.feature-header')).getText();
+  }
+
+  getCalcTipInputLabel() {
+    return element(by.css('label[for="tipPercent"]'));
+  }
+
+  getCalcTipInputLabelText() {
+    return this.getCalcTipInputLabel().getText();
+  }
+
+  getCalcTipInput() {
+    return element(by.css('input#tipPercent'));
+  }
+
+  getCalcTipInputType() {
+    return this.getCalcTipInput().getAttribute('type');
+  }
+
+  getCalcTipInputStep() {
+    return this.getCalcTipInput().getAttribute('step');
+  }
+
+  getCalcTipInputModelBinding() {
+    return this.getCalcTipInput().getAttribute('formControlName');
+  }
+
+  getCalcPriceInputLabel() {
+    return element(by.css('label.amount-input-label[for="price"]'));
+  }
+
+  getCalcPriceInputLabelText() {
+    return this.getCalcPriceInputLabel().getText();
+  }
+
+  getCalcPriceInput() {
+    return element(by.css('input#price'));
+  }
+
+  getCalcPriceInputType() {
+    return this.getCalcPriceInput().getAttribute('type');
+  }
+
+  getCalcPriceInputStep() {
+    return this.getCalcPriceInput().getAttribute('step');
+  }
+
+  getCalcPriceInputModelBinding() {
+    return this.getCalcPriceInput().getAttribute('formControlName');
   }
 }
