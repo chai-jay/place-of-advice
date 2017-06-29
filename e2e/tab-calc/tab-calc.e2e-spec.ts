5ab90848433ffb9f2929f6a290c7f4ee8dd707a2
Jing Chai <jingweichai@gmail.com>
2017-06-29 19:44:05 -0400

    Add e2e test for Tab Calc page's header and inputs.

--- e2e/tab-calc/tab-calc.e2e-spec.ts
+++ e2e/tab-calc/tab-calc.e2e-spec.ts
@@ -8,14 +8,47 @@ describe('place-of-advice App', () => {
   });
 
   describe('Tab-Calc Component', () => {
-    it ('should exist', () => {
+    it('should exist', () => {
       page.navigateTo()
-      expect(page.getTabCalcElement().isPresent()).toBe(true);
+      expect(page.getCalcElement().isPresent()).toBe(true);
     });
 
-    it ('should use an md-card element', () => {
+    it('should use an md-card element', () => {
       page.navigateTo()
-      expect(page.getTabCalcMdcardElement().isPresent()).toBe(true);
+      expect(page.getCalcMdcardElement().isPresent()).toBe(true);
+    });
+
+    it('should have title text', () => {
+      page.navigateTo();
+      expect(page.getCalcHeaderText()).toBe('NJ Tab Calculator');
+    })
+
+    it('should have tip percent input label with correct text', () => {
+      page.navigateTo();
+      expect(page.getCalcTipInputLabel().isPresent()).toBe(true);
+      expect(page.getCalcTipInputLabelText()).toBe('Tip Percent');
+    });
+
+    it('should have a tip percent input with correct attributes', () => {
+      page.navigateTo();
+      expect(page.getCalcTipInput().isPresent());
+      expect(page.getCalcTipInputType()).toBe('number');
+      expect(page.getCalcTipInputStep()).toBe('1');
+      expect(page.getCalcTipInputModelBinding()).toBe('tipPercent');
+    });
+
+    it('should have price input label with correct text', () => {
+      page.navigateTo();
+      expect(page.getCalcPriceInputLabel().isPresent()).toBe(true);
+      expect(page.getCalcPriceInputLabelText()).toBe('Price (pretax)');
+    });
+
+    it('should have a tip percent input with correct attributes', () => {
+      page.navigateTo();
+      expect(page.getCalcPriceInput().isPresent());
+      expect(page.getCalcPriceInputType()).toBe('number');
+      expect(page.getCalcPriceInputStep()).toBe('0.01');
+      expect(page.getCalcPriceInputModelBinding()).toBe('price');
     });
   });
 
