d742a2634d6a32c93d65d218041511894e7c42fb
Jing Chai <jingweichai@gmail.com>
2017-07-30 22:09:33 -0400

    Add specs for ReactiveForms in TabCalcComponent unit test.

--- src/app/tab-calc/tab-calc/tab-calc.component.spec.ts
+++ src/app/tab-calc/tab-calc/tab-calc.component.spec.ts
@@ -30,4 +30,44 @@ describe('TabCalcComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should be initialized with correct values', () => {
+    expect(component.tabCalcForm.value['price']).toBe(0);
+    expect(component.tabCalcForm.value['tipPercent']).toBe(20);
+    expect(component.taxAmount).toBe(0);
+    expect(component.tipAmount).toBe(0);
+    expect(component.totalAmount).toBe(0);
+    expect(component.taxRate).toBe(0.07);
+  });
+
+  it('should update the amounts correctly when price is changed', () => {
+    const specPrice = 100.00;
+    component.tabCalcForm.patchValue({price: specPrice});
+
+    expect(component.taxAmount).toBe(specPrice * component.taxRate);
+    expect(component.tipAmount).toBe(specPrice * component.tabCalcForm.value['tipPercent'] / 100);
+    expect(component.totalAmount).toBe(specPrice + component.taxAmount + component.tipAmount);
+  });
+
+  it('should update the amounts correctly when tip percent is changed', () => {
+    const specPrice = 100.00;
+    const specTipPercent = 10;
+    component.tabCalcForm.patchValue({price: specPrice, tipPercent: specTipPercent});
+
+    expect(component.taxAmount).toBe(specPrice * component.taxRate);
+    expect(component.tipAmount).toBe(specPrice * specTipPercent / 100);
+    expect(component.totalAmount).toBe(specPrice + component.taxAmount + component.tipAmount);
+  });
+
+  it('should reset the form values when resetForm is called', () => {
+    component.tabCalcForm.patchValue({price: 123, tipPercent: 23});
+    component.resetForm();
+
+    expect(component.tabCalcForm.value['price']).toBe(0);
+    expect(component.tabCalcForm.value['tipPercent']).toBe(20);
+    expect(component.taxAmount).toBe(0);
+    expect(component.tipAmount).toBe(0);
+    expect(component.totalAmount).toBe(0);
+    expect(component.taxRate).toBe(0.07);
+  });
 });
