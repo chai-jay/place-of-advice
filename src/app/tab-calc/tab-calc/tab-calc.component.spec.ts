e75559cfd04f27d7a30eaeb8b27ff0687cda8c51
Jing Chai <jingweichai@gmail.com>
2017-07-30 20:48:08 -0400

    Fix TabCalcCOmponent unit test.

--- src/app/tab-calc/tab-calc/tab-calc.component.spec.ts
+++ src/app/tab-calc/tab-calc/tab-calc.component.spec.ts
@@ -1,25 +1,33 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
 
-// import { TabCalcComponent } from './tab-calc.component';
+import { TabCalcComponent } from './tab-calc.component';
 
-// describe('TabCalcComponent', () => {
-//   let component: TabCalcComponent;
-//   let fixture: ComponentFixture<TabCalcComponent>;
+describe('TabCalcComponent', () => {
+  let component: TabCalcComponent;
+  let fixture: ComponentFixture<TabCalcComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ TabCalcComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        BrowserAnimationsModule, // Needed by MaterialModule
+        MaterialModule,
+        ReactiveFormsModule
+      ],
+      declarations: [TabCalcComponent]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(TabCalcComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TabCalcComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should be created', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
